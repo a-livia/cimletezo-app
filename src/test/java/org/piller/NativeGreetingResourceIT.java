ccc2842de3f9c2eaa4f6335af2c2017020b69140
quarkusio <no-reply@quarkus.io>
2021-05-20 14:48:21 +0000

    Initial commit

---
+++ src/test/java/org/piller/NativeGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package org.piller;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
